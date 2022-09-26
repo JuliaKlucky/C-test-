ecb2ea3e402440fe219803e419df83c4075942e7
Lucky <suspected.nyan@gmail.com>
2022-09-26 19:13:01 +0700

    массивы и функции

---
+++ 10/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {18, 22, 34, 68, 15, 6, 97, 68};
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
